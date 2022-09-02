ca20ab50339324e3c2a3d23ce829ca4fd8a4cafa
kotatsuko <107920100+kotatsuko@users.noreply.github.com>
2022-09-02 01:42:18 +0000

    gemの導入と、devise関連のmodel,controller,viewの準備と、controllerとviewのadmin,publicファイルの準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  
+  devise_for :admins, controllers: {
+  sessions: "admin/sessions"
+  }
+  
+  devise_for :end_users, controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
